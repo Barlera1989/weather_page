cedb5800cc091285fbbc657ada6b9440c957c5bd
rafael <barlera1989@gmail.com>
2021-02-15 23:20:47 -0300

    feature: new layout with responsivity

---
+++ src/components/pages/constants.js
@@ -0,0 +1,9 @@
+const pageTexts = {
+  title: 'Weather buddy',
+  textBeforeInput: 'How is the weather in',
+  textAfterInput: 'now?',
+  errorMessage: "Sorry. We couldn't find the specified city",
+
+}
+
+export default pageTexts
